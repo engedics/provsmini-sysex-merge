5a36390783ec13b8d4fcbecb0d75e13e284dd2c4
engedics <engedi.cs@gmail.com>
2025-07-23 09:54:59 +0200

    Improve logging and file checks

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# SysEx preset merge tool for the Behringer Pro VS Mini synthesizer
+# SysEx preset combiner tool for the Behringer Pro VS Mini synthesizer
 A tool for combining/merging SysEx **preset** files for bulk upload to the Behringer Pro VS Mini synthesizer.
 
 ## Why
@@ -11,18 +11,17 @@ The first 8 bytes and last 2 bytes of each SysEx preset file are stripped away. 
 
 To get an overview of the supported arguments:
 ```shell
-python3 provsmini_sysex_merge.py --help
+python3 provsmini_preset_combiner.py --help
 ```
 
 To run in its simplest form:
 ```shell
-python3 provsmini_sysex_merge.py /path/to/your/collection
+python3 provsmini_preset_combiner.py /path/to/your/collection
 ```
 
-Then, upload the result file (by default: combined.syx in the source folder) with SynthTribe (Send -> All Presets).
+Then, upload the output file (by default: combined.syx in the source folder) with SynthTribe (Send -> All Presets).
 
 ## Disclaimer
 I'm not responsible for any damage inflicted upon the synthesizer! I've tried a few combinations, and they all uploaded fine, although I couldn't figure out the role of the first byte of the terminator.
 
-**UPDATE** It's a checksum, added a [calculation](https://github.com/RomanKubiak/ctrlr/discussions/634#discussioncomment-9183868) for it.
-
+**UPDATE:** It's a checksum, added a [calculation](https://github.com/RomanKubiak/ctrlr/discussions/634#discussioncomment-9183868) for it.
--- provsmini_preset_combiner.py
+++ provsmini_preset_combiner.py
@@ -8,11 +8,11 @@ TERMINATOR = bytes.fromhex('F7')
 
 def parse_arguments():
     parser = argparse.ArgumentParser(
-        description='Combines/merges SysEx preset files for bulk transmission to the Behringer Pro VS Mini synthesizer.')
+        description='Combines SysEx preset files for bulk transmission to the Behringer Pro VS Mini synthesizer.')
 
     parser.add_argument(
         'directory',
-        help='Directory to get the preset files from. Only the first 32 files are merged!')
+        help='Directory to get the preset files from. Only the first 32 files are combined!')
     parser.add_argument(
         '-e', '--extension',
         default='syx',
@@ -29,35 +29,32 @@ def parse_arguments():
     return parser.parse_args()
 
 
-def parse_files(directory_path, extension):
+def combine_input(input_path, extension):
     sysex_contents = []
 
-    sysex_filepaths = list(directory_path.glob(f'*.{extension}'))
+    sysex_filepaths = list(input_path.glob(f'*.{extension}'))
     if len(sysex_filepaths) == 0:
         raise FileNotFoundError('No matching files found!')
-    logging.info(f'Found {len(sysex_filepaths)} sysex files in {directory_path.absolute()}')
-    logging.debug(f'Sysex files found:\n{'\n'.join(sysex_filepath.as_posix() for sysex_filepath in sysex_filepaths)}')
+    logging.info(f'Found {len(sysex_filepaths)} SysEx files in {input_path.absolute()}')
+    logging.debug(f'SysEx files found:\n{'\n'.join(sysex_filepath.as_posix() for sysex_filepath in sysex_filepaths)}')
 
     for sysex_filepath in sysex_filepaths:
         sysex_content = sysex_filepath.read_bytes()
         if sysex_content.startswith(PREAMBLE):
-            logging.info(f'Skipping combined file {sysex_filepath.name}')
+            logging.warning(f'Skipping combined file {sysex_filepath.name}')
             continue
-        logging.info(f'Adding {sysex_filepath.name}')
+        if not (sysex_content.startswith(PREAMBLE[:-1]) and sysex_content.endswith(TERMINATOR) and len(sysex_content) == 75):
+            logging.warning(f'Skipping file with unrecognized format: {sysex_filepath.name}')
+            continue
+        preset_name = sysex_content[63:71].decode('ascii').rstrip()
+        if len(sysex_contents) == 32:
+            logging.warning(f'Skipping file above the 32-preset limit: {sysex_filepath.name} ({preset_name})')
+            continue
+        logging.info(f'Adding {sysex_filepath.name} ({preset_name})')
         logging.debug(f'File contents:\n{sysex_content.hex()}')
-        sysex_contents.append(sysex_content)
+        sysex_contents.append(sysex_content[8:-3])
 
-    return sysex_contents[:32]
-
-
-def combine_files(sysex_contents):
-    combined_contents = []
-    for sysex_content in sysex_contents:
-        sysex_stripped = sysex_content[8:-3]
-        logging.debug(f'Stripped contents: {sysex_stripped.hex()}')
-        combined_contents.append(sysex_stripped)
-
-    return bytes().join(combined_contents)
+    return bytes().join(sysex_contents)
 
 
 def calculate_checksum(combined_contents):
@@ -67,29 +64,30 @@ def calculate_checksum(combined_contents):
 
 def main():
     args = parse_arguments()
-    sysex_bytes = []
+    output_bytes = []
 
     logging.basicConfig(
         format='%(levelname)s: %(message)s' if args.verbose else '%(message)s',
         level=logging.DEBUG if args.verbose else logging.INFO)
 
-    directory_path = Path(args.directory)
-    sysex_contents = parse_files(directory_path, args.extension)
-
     logging.debug(f'Adding preamble: {PREAMBLE.hex()}')
-    sysex_bytes.append(PREAMBLE)
-    combined_contents = combine_files(sysex_contents)
-    logging.debug(f'Adding combined bytes: {combined_contents.hex()}')
-    sysex_bytes.append(combined_contents)
-    checksum = calculate_checksum(combined_contents)
+    output_bytes.append(PREAMBLE)
+
+    input_path = Path(args.directory)
+    sysex_contents = combine_input(input_path, args.extension)
+    logging.debug(f'Adding combined bytes: {sysex_contents.hex()}')
+    output_bytes.append(sysex_contents)
+
+    checksum = calculate_checksum(sysex_contents)
     logging.debug(f'Adding calculated checksum: {checksum.hex()}')
-    sysex_bytes.append(checksum)
+    output_bytes.append(checksum)
+
     logging.debug(f'Adding terminator: {TERMINATOR.hex()}')
-    sysex_bytes.append(TERMINATOR)
+    output_bytes.append(TERMINATOR)
 
     output_path = Path(args.output or f'{args.directory}/combined.syx')
     logging.info(f'Writing the combined SysEx to {output_path.absolute()}')
-    output_path.write_bytes(bytes().join(sysex_bytes))
+    output_path.write_bytes(bytes().join(output_bytes))
     logging.info('Done!')
 
 
